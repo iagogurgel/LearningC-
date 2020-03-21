a4a16b5f6d5061fb2b0b3d4b904101dae4ea12bc
Iago Gurgel <iago.gurgel@academico.ifpb.edu.br>
2020-03-21 15:52:58 -0300

    Organizada e adicionada novas funções para a calculadora

--- Calculadora/main.cpp
+++ Calculadora/main.cpp
@@ -9,58 +9,78 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    int num1;
-    int num2;
+    float num1;
+    float num2;
     char op;
     char op2;
     float resultado;
 
-    cout << "Você deseja utilizar a calculadora, se sim digite 'S'." << endl;
-    cout << "Se não deseja utilizar, digite 'N'." << endl;
+    cout << "Você deseja utilizar a calculadora[S/N]" << endl;
     cin >> op;
     system("CLS");
 
-    while (op == 's' || op == 'S'){
+    while (op == 's' || op == 'S')
+        {
             cout << "Digite um número: " << endl;
             cin >> num1;
             system("CLS");
-            cout << "Digite outro número: " << endl;
-            cin >> num2;
-            system("CLS");
-            cout << "Se deseja somar os números, digite '+'." << endl;
-            cout << "Se deseja subtrair os números, digite '-'." << endl;
-            cout << "Se deseja multiplicar os números, digite '*'." << endl;
-            cout << "Se deseja dividir os números, digite '/'." << endl;
-            cout << "Se deseja potencializar os números, digite 'P'." << endl;
-            cout << "Se deseja radicalizar o primeiro número, digite 'R'." << endl;
+            cout << "Digite a operação que deseja utilizar " << endl;
+            cout << "[+] para somar" << endl;
+            cout << "[-] para subtrair" << endl;
+            cout << "[*] para multiplicar" << endl;
+            cout << "[/] para dividir" << endl;
+            cout << "[^] para potenciação" << endl;
+            cout << "[Q] para raiz quadrada" << endl;
+            cout << "[C] para raiz cúbica" << endl;
             cin >> op2;
             system("CLS");
             switch (op2){
             case '+':
+                cout << "Digite o segundo número: " << endl;
+                cin >> num2;
+                system("CLS");
                 resultado = num1 + num2;
-                cout << "O resultado é: " << resultado << endl;
+                cout << "O resultado da soma é: " << resultado << endl;
                 break;
             case '-':
+                cout << "Digite o segundo número: " << endl;
+                cin >> num2;
+                system("CLS");
                 resultado = num1 - num2;
-                cout << "O resultado é: " << resultado << endl;
+                cout << "O resultado da subtração é: " << resultado << endl;
                 break;
             case '*':
+                cout << "Digite o segundo número: " << endl;
+                cin >> num2;
+                system("CLS");
                 resultado = num1 * num2;
-                cout << "O resultado é: " << resultado << endl;
+                cout << "O resultado da multiplicação é: " << resultado << endl;
                 break;
             case '/':
-                resultado = (float)num1 / num2;
-                cout << "O resultado é: " << resultado << endl;
+                cout << "Digite o segundo número: " << endl;
+                cin >> num2;
+                system("CLS");
+                resultado = num1 / num2;
+                cout << "O resultado da divisão é: " << resultado << endl;
                 break;
-            case 'P':
-            case 'p':
-                resultado = pow((double)num1,(double)num2);
-                cout << "O resultado é: " << resultado << endl;
+            case '^':
+                cout << "Digite o segundo número: " << endl;
+                cin >> num2;
+                system("CLS");
+                resultado = pow(num1,num2);
+                cout << "O resultado da potência é: " << resultado << endl;
                 break;
-            case 'R':
-            case 'r':
+            case 'Q':
+            case 'q':
+                system("CLS");
                 resultado = sqrt(num1);
-                cout << "O resultado é: " << resultado << endl;
+                cout << "O resultado do radical é: " << resultado << endl;
+                break;
+            case 'C':
+            case 'c':
+                system("CLS");
+                resultado = pow(num1, 1.0/3.0);
+                cout << "O resultado do radical é: " << resultado << endl;
                 break;
             default:
                 cout << "Não foi possível realizar esta operação.";
@@ -69,10 +89,11 @@ int main()
 
 
 
-            }
+        }
 
-            cout << "Você ainda quer continuar a usar a calculadora?" << endl;
+            cout << "Você ainda quer continuar a usar a calculadora? [S/N]" << endl;
             cin >> op;
+            system("CLS");
 
     }
     return 0;
